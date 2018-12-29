d7900e3cc6653431545568a9608c51cdc22f69cb
s-yoshihara <s-yoshihara@saitama-tec.co.jp>
2018-12-29 07:38:45 +0000

    file add

---
+++ functions.ts
@@ -0,0 +1,25 @@
+export function square_walk(
+    input:number[],
+    rowBeginfunc:(
+      y:number
+      )=>void
+    =(y)=>console.log(`BEGIN[${y}]`)
+    ,cellfunc:(
+          x:number,
+          y:number,
+         )=>void 
+    =(x,y)=>console.log(`[${x}:${y}]`)
+    ,
+    rowEndfunc:(
+            y:number
+           )=>void
+    =(y)=>console.log(`END[${y}]`)
+  ){
+    input.forEach((D:number,Y)=>{
+        rowBeginfunc(Y);
+        input.forEach((D:number,X)=>{
+            cellfunc(X,Y);
+            });
+        rowEndfunc(Y)
+        });
+  }
