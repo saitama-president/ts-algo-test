a282f32f912e009195f8db4c00b6099a95261773
s-yoshihara <s-yoshihara@saitama-tec.co.jp>
2019-01-07 05:18:03 +0000

    テストできるように修正

---
+++ tests/sample-test.ts
@@ -0,0 +1,20 @@
+import {
+  describe,it
+} from "mocha"
+import {
+  assert
+} from "chai"
+import * as dotenv from "dotenv";
+
+
+dotenv.config();
+
+describe("TEST",()=>{
+  it("IT",
+    ()=>{
+    assert(true);
+    
+  });
+
+});
+
